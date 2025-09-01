d100c8a981fa7e4834b21e030ab6720f96ebcd3a
EmanAyman256 <emanayman256@gmail.com>
2025-09-01 13:22:11 +0000

    updated copy logic and add the export as excel sheet function and search logic

--- app/project1/webapp/controller/ServiceMaster.controller.js
+++ app/project1/webapp/controller/ServiceMaster.controller.js
@@ -4,7 +4,9 @@ sap.ui.define([
     "sap/m/VBox",
     "sap/m/Text",
     "sap/m/Button",
-], function (Controller, Dialog, Text, VBox, Button) {
+    "sap/ui/export/Spreadsheet",
+    "sap/ui/export/library"
+], function (Controller, Dialog, VBox, Text, Button, Spreadsheet, exportLibrary) {
     "use strict";
 
     return Controller.extend("project1.controller.ServiceMaster", {
@@ -100,9 +102,6 @@ sap.ui.define([
             this._oEditDialog.open();
         },
 
-        // onEdit: function (oEvent) {
-        //     // Logic to edit service type
-        // },
         onDelete: function (oEvent) {
             // Logic to delete service type
         },
@@ -165,34 +164,152 @@ sap.ui.define([
             this._oValueHelpDialog.open();
         },
         onCopy: function () {
+            var oTable = this.byId("serviceMaster");
+            var oSelectedItem = oTable.getSelectedItem();
+
+            if (!oSelectedItem) {
+                sap.m.MessageBox.warning("Please, select an item to copy");
+                return;
+            }
+
+            var oContext = oSelectedItem.getBindingContext();
+            var oSelectedData = oContext.getObject();
+            var oModel = this.getView().getModel();
+            var aData = oModel.getProperty("/ServiceMaster");
+
+            // Create a new object as a copy
+            var oNewData = Object.assign({}, oSelectedData);
+            oNewData.Code = oNewData.Code + "_Copy"; // give it a unique code
+
+            // Create Copy Dialog if not exists
+            if (!this._oCopyDialog) {
+                this._oCopyDialog = new sap.m.Dialog({
+                    title: "Copy Service Master",
+                    titleAlignment: "Center",
+                    contentWidth: "600px",
+                    content: new sap.m.VBox({}),
+                    beginButton: new sap.m.Button({
+                        text: "Save",
+                        type: "Emphasized",
+                        press: () => {
+                            var aContent = this._oCopyDialog.getContent()[0].getItems();
+
+                            var oNewEntry = {
+                                Code: aContent[1].getValue(),
+                                SearchTerm: aContent[3].getValue(),
+                                Description: aContent[5].getValue(),
+                                lastChangeDate: aContent[7].getValue(),
+                                serviceType: aContent[9].getValue(),
+                                CreatedOn: aContent[11].getValue()
+                            };
+
+                            // Add new entry to the array
+                            aData.push(oNewEntry);
+                            oModel.setProperty("/ServiceMaster", aData);
+                            oModel.refresh(true);
+
+                            this._oCopyDialog.close();
+                            sap.m.MessageToast.show("Item copied successfully");
+                        }
+                    }),
+                    endButton: new sap.m.Button({
+                        text: "Cancel",
+                        press: () => this._oCopyDialog.close()
+                    })
+                });
+
+                this.getView().addDependent(this._oCopyDialog);
+            }
+
+            // Fill dialog with copied data
+            this._oCopyDialog.removeAllContent();
+            this._oCopyDialog.addContent(
+                new sap.m.VBox({
+                    items: [
+                        new sap.m.Label({ text: "Service Master Code", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.Code }),
+
+                        new sap.m.Label({ text: "Search Term", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.SearchTerm }),
+
+                        new sap.m.Label({ text: "Description", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.Description }),
+
+                        new sap.m.Label({ text: "Last Changed Date", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.lastChangeDate }),
+
+                        new sap.m.Label({ text: "Service Type", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.serviceType }),
+
+                        new sap.m.Label({ text: "Created On", design: "Bold" }),
+                        new sap.m.Input({ value: oNewData.CreatedOn })
+                    ]
+                })
+            );
+
+            this._oCopyDialog.open();
+        },
+        onExport: function () {
+            var oModel = this.getView().getModel();
+            var aData = oModel.getProperty("/ServiceMaster");
+
+            if (!aData || aData.length === 0) {
+                sap.m.MessageBox.warning("No data available to export");
+                return;
+            }
+
+            // Define the Excel columns
+            var aColumns = [
+                { label: "Service Master Code", property: "Code" },
+                { label: "Search Term", property: "SearchTerm" },
+                { label: "Description", property: "Description" },
+                { label: "Last Changed Date", property: "lastChangeDate" },
+                { label: "Service Type", property: "serviceType" },
+                { label: "Created On", property: "CreatedOn" }
+            ];
+
+            // Settings for Spreadsheet
+            var oSettings = {
+                workbook: { columns: aColumns },
+                dataSource: aData,
+                fileName: "ServiceMaster.xlsx",
+                worker: false // set to true for large datasets
+            };
+
+            var oSheet = new Spreadsheet(oSettings);
+            oSheet.build()
+                .then(() => {
+                    sap.m.MessageToast.show("Export finished!");
+                })
+                .finally(() => {
+                    oSheet.destroy();
+                });
+        },
+        onSearch: function (oEvent) {
+    var sQuery = oEvent.getParameter("newValue"); // النص اللي اتكتب في SearchField
     var oTable = this.byId("serviceMaster");
-    var oSelectedItem = oTable.getSelectedItem();
+    var oBinding = oTable.getBinding("items");
 
-    if (!oSelectedItem) {
-        sap.m.MessageBox.warning("Please, select an item to copy");
-        return;
-    }
+    if (sQuery && sQuery.length > 0) {
+        // فلترة على أكتر من عمود
+        var oFilter1 = new sap.ui.model.Filter("Code", sap.ui.model.FilterOperator.Contains, sQuery);
+        var oFilter2 = new sap.ui.model.Filter("SearchTerm", sap.ui.model.FilterOperator.Contains, sQuery);
+        var oFilter3 = new sap.ui.model.Filter("Description", sap.ui.model.FilterOperator.Contains, sQuery);
 
-    var oContext = oSelectedItem.getBindingContext();
-    var oSelectedData = oContext.getObject();
-    var oModel = this.getView().getModel();
-    var aData = oModel.getProperty("/ServiceMaster");
+        var oCombinedFilter = new sap.ui.model.Filter({
+            filters: [oFilter1, oFilter2, oFilter3],
+            and: false // OR logic
+        });
 
-    // Create a copy (shallow clone of object)
-    var oNewData = Object.assign({}, oSelectedData);
-
-    // Optional: change the Code so it doesn’t conflict
-    oNewData.Code = oNewData.Code + "_Copy";
-
-    // Add new record to the array
-    aData.push(oNewData);
-
-    // Update model
-    oModel.setProperty("/ServiceMaster", aData);
-    oModel.refresh(true);
-
-    sap.m.MessageToast.show("Item copied successfully");
+        oBinding.filter([oCombinedFilter]);
+    } else {
+        oBinding.filter([]); 
+     }
 }
+
+
+
+
 
 
 
