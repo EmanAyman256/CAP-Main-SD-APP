1ba0cf4e1e828343bab1b8f39785c254e2f0a0f2
EmanAyman256 <emanayman256@gmail.com>
2025-09-16 23:18:49 +0000

    add initial data and remove unused files

--- app/project1/webapp/controller/Example.controller.js
+++ app/project1/webapp/controller/Example.controller.js
@@ -1,27 +1,129 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "sap/m/MessageToast",
+  "sap/m/MessageBox",
+  "sap/m/Dialog",
+  "sap/m/Label",
+  "sap/m/Input",
+  "sap/m/Button"
+], function (Controller, JSONModel, MessageToast, MessageBox, Dialog, Label, Input, Button) {
   "use strict";
 
   return Controller.extend("project1.controller.Example", {
     onInit: function () {
-      var oModel = new JSONModel();
+      this.oModel = new JSONModel();
+      this.getView().setModel(this.oModel);
+      this.loadServiceNumbers();
+    },
 
-      // Fetch data from CAP OData service
-      fetch("/odata/v4/SalesCloudService/ServiceNumbers")
-        .then(response => response.json())
+    // Load all data
+    loadServiceNumbers: function () {
+      fetch("/odata/v4/sales-cloud/ServiceNumbers")
+        .then(res => res.json())
         .then(data => {
-          console.log(data.value);
-          
-            debugger
-          // Wrap array inside an object for binding
-          oModel.setData({ ServiceTypes: data.value });
-          this.getView().byId("serviceTypesTable").setModel(oModel);
+          this.oModel.setData({ ServiceNumbers: data.value });
         })
-        .catch(err => {
-          console.error("Error fetching ServiceTypes", err);
-        });
+        .catch(err => console.error("Error fetching:", err));
+    },
+
+    // ADD
+    onAdd: function () {
+      const newEntry = {
+        serviceNumberCode: "SN" + Date.now(),
+        description: "New Service Number",
+        createdBy: "UI5"
+      };
+
+      fetch("/odata/v4/sales-cloud/ServiceNumbers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newEntry)
+      })
+      .then(() => {
+        MessageToast.show("Service Number Added");
+        this.loadServiceNumbers();
+      });
+    },
+
+    // UPDATE
+    onUpdate: function () {
+      const oTable = this.byId("serviceNumbersTable");
+      const oSelected = oTable.getSelectedItem();
+      if (!oSelected) {
+        MessageBox.warning("Select a row to update");
+        return;
+      }
+
+      const data = oSelected.getBindingContext().getObject();
+
+      // Create popup dialog
+      const oDialog = new Dialog({
+        title: "Update Service Number",
+        content: [
+          new Label({ text: "Code" }),
+          new Input({ value: data.serviceNumberCode, editable: false }),
+          new Label({ text: "Description" }),
+          new Input("descInput", { value: data.description }),
+          new Label({ text: "Created By" }),
+          new Input("createdByInput", { value: data.createdBy })
+        ],
+        beginButton: new Button({
+          text: "Save",
+          press: () => {
+            const updatedEntry = {
+              serviceNumberCode: data.serviceNumberCode,
+              description: sap.ui.getCore().byId("descInput").getValue(),
+              createdBy: sap.ui.getCore().byId("createdByInput").getValue()
+            };
+
+            fetch(`/odata/v4/sales-cloud/ServiceNumbers('${data.serviceNumberCode}')`, {
+              method: "PATCH",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(updatedEntry)
+            })
+            .then(() => {
+              MessageToast.show("Service Number Updated");
+              this.loadServiceNumbers();
+            });
+
+            oDialog.close();
+          }
+        }),
+        endButton: new Button({
+          text: "Cancel",
+          press: () => oDialog.close()
+        }),
+        afterClose: () => oDialog.destroy()
+      });
+
+      oDialog.open();
+    },
+
+    // DELETE
+    onDelete: function () {
+      const oTable = this.byId("serviceNumbersTable");
+      const oSelected = oTable.getSelectedItem();
+      if (!oSelected) {
+        MessageBox.warning("Select a row to delete");
+        return;
+      }
+
+      const data = oSelected.getBindingContext().getObject();
+
+      MessageBox.confirm(`Delete Service Number '${data.serviceNumberCode}'?`, {
+        onClose: (sAction) => {
+          if (sAction === "OK") {
+            fetch(`/odata/v4/sales-cloud/ServiceNumbers('${data.serviceNumberCode}')`, {
+              method: "DELETE"
+            })
+            .then(() => {
+              MessageToast.show("Service Number Deleted");
+              this.loadServiceNumbers();
+            });
+          }
+        }
+      });
     }
   });
 });
